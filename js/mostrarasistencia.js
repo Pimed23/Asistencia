b87d0e7d9047ce864c3e51a4bf8f7bdea6f875c5
LeDaVR <42551056+LeDaVR@users.noreply.github.com>
2020-08-11 11:07:20 -0500

    Create mostrarasistencia.js

---
+++ js/mostrarasistencia.js
@@ -0,0 +1,20 @@
+function obtenerCantidadFaltas() {
+    var correo = document.getElementById('correo').value;
+    var datos = 'correo='+correo;
+    $.ajax({
+        type: 'POST',
+        url: "php/TotalFaltas.php",
+        data: datos,
+        success: function( response ) {
+            if( response == "0") {
+                alert("No se encontro persona");
+            } else {
+                var cursos = JSON.parse( response );
+                cargarAsitencia( asistencia );
+            }
+        }, error: function( response, status, error ) {
+            alert("No encontrado");
+        }
+    });
+    return false;
+}
